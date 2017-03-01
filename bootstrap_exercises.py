06630d507638e7c20d4cade31b4a294e9ea7f798
Jimmy Nguyen <jnguyen75@gmail.com>
2017-03-01 03:19:44 -0800

    Added 'exercises' key to exercises.json

--- bootstrap_exercises.py
+++ bootstrap_exercises.py
@@ -5,6 +5,6 @@ import models
 session = models.Session()
 
 exercises = json.load(open('exercises.json', 'r'))
-for exercise in exercises:
+for exercise in exercises['exercises']:
     session.add(models.Exercise(**exercise))
 session.commit()
