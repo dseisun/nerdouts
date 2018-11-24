b1e9d3325562d4e0430177d1ca65d1f183aae8ce
Jimmy Nguyen <jnguyen75@gmail.com>
2018-11-24 09:37:28 -0800

    Refactor: Decouple MusicIface & say() and models & run() (#11)
    Moved logger from class to module
    
    Moved MusicIface related calls from workout to models
    
    Refactored MusicIface to be Singleton class instead of cached_property
    
    Moved MusicIface to music.py
    
    Moved the run() methods from models.py to workout.py
    
    Added Coach class to decouple speech from MusicIFace
    
    Renamed MusicIface to Player
    
    Removed extra/unused database connection parameter
    
    Moved YAML loading and engine to new database.py file
    
    Moved the scoped session maker to database.py (fixes bootstrap_exercises.py)
    
    Some bugfixes from testing
    
    * Removed some accidentally committed diff stuff
    * Some things needed to be static methods based on how they were being
    called
    * Fixed imports
    * Make everything inherit from object

--- workout.py
+++ workout.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import argparse
+from datetime import datetime
 import json
 from models import Workout, Exercise, ExerciseCategory, WorkoutExercise
+import time
 from random import random, sample
-import subprocess
 from itertools import groupby, count
+from database import Session
 import logging
-from database import engine, Session
+
+from coach import Coach
+from music import Player
 
 logging.basicConfig(level=logging.INFO)
 
@@ -59,22 +63,49 @@ def config_to_category_times(session, config_path):
     return category_times
 
 
+def countdown(exc_time):
+    for sec in range(exc_time):
+        logging.info("%d seconds until the next exercise" % (exc_time - sec))
+        if exc_time - sec == 10:
+            Coach.say("10 Seconds left")
+        time.sleep(1)
+
+
+def run_exercise(exercise):
+    for side in exercise.sides:
+        for i in range(exercise.repetition):
+            Player.pause()
+            Coach.say(exercise.sentence(side=side))
+            Player.play()
+            countdown(exercise.default_time)
+
+
+def run_workout(workout, debug=False):
+    Player.next()
+    for e in workout.workout_exercises:
+        e.created_date = datetime.now()
+        e.time_per_set = e.exercise.default_time
+        e.repetition = e.exercise.repetition
+        if not debug:
+            run_exercise(e.exercise)
+        else:
+            logging.info("QA MODE: Running exercise: %s" % e)
+            time.sleep(1)
+    Coach.say('Workout finished')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="Daniels workouts")
     parser.add_argument('-t', dest='time', type=int, help='Length (of time) of workout in minutes', required=True)
     parser.add_argument('-w', dest='workout', type=str, help='Workout config JSON', nargs='?', default='config.json')
     parser.add_argument('-d', dest='debug', action='store_true', help='Run in debug mode.')
     args = parser.parse_args()
-
-    subprocess.call('clementine -l /home/dseisun/.config/Clementine/Playlists/Workout.xspf', shell=True)
-
     ses = Session()
-
     gw = WorkoutGenerator()
     category_times = config_to_category_times(ses, args.workout)
     wo = gw.generate_workout(category_times)
 
-    wo.iface.Next()
-    wo.run_workout(args.debug)
+    run_workout(wo, args.debug)
+
     ses.add(wo)
     ses.commit()
