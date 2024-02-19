c4df3b9bfa0831ac13023f158438763e887607ee
Daniel Seisun <daseisun@gmail.com>
2024-02-19 15:44:31 -0500

    Wire up static exercises to work by breaking dynamic workout. Added some basic tests and fixed tts and music player

--- nerdouts/static_workouts.py
+++ nerdouts/static_workouts.py
@@ -1,5 +1,16 @@
+from functools import partial
+from typing import List
+from generate_static_workout import StaticExercise, get_exercise_by_name, load_exercises_from_json
 
-workouts = {
-    'name': "",
-    'workout': ...
+get_by_name = partial(get_exercise_by_name, exercises=load_exercises_from_json())
+
+workouts: dict[str, List[StaticExercise]] = {
+    'workout': [
+        get_by_name("Clamshells"),
+        get_by_name("High Kicks"),
+        get_by_name("Curls"),
+        get_by_name("Calf Stretch"),
+        get_by_name("Jump rope"),
+        get_by_name("Narrow Pushups")
+    ]
 }
